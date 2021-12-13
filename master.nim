2e54c05b96bccfa04114dab977a8aacb53791365
Duskhorn <uwueclipsedhorn@gmail.com>
2021-12-13 12:30:36 +0100

    more server implementations

--- master.nim
+++ master.nim
@@ -1,13 +1,13 @@
 import std/[asyncnet, asyncdispatch]
 from times import get_clock_str, get_date_str, now, DateTime
 from strutils import isEmptyOrWhitespace
-
-const port = 42068 #todo: static load from config file
+from parseutils import parse_int
+from os import paramStr, paramCount
 
 var clients {. threadvar .}: seq[AsyncSocket] 
 var curr_time: DateTime
 
-proc serve(port: int, ip: string = "") {. async .} =
+proc init_server(port: int, ip: string): AsyncSocket = 
     
     let server = newAsyncSocket()
 
@@ -15,7 +15,7 @@ proc serve(port: int, ip: string = "") {. async .} =
     server.bindAddr(Port(port), ip)
     server.listen()
 
-    #todo: setup ping socket
+    #TODO: setup ping socket
 
     curr_time = now()
     let time = get_date_str(curr_time) & " " & get_clock_str(curr_time)
@@ -26,11 +26,44 @@ proc serve(port: int, ip: string = "") {. async .} =
 
 
     echo "*** starting master server on " & currip & ":" & $port & " at " & time
+    return server
+
+
+proc serve(port: int, ip, dir: string) {. async .} =
+
+    let server = init_server(port, ip)
+
+    var
+        reload_cfg = true
+        cfg_name = ""
+        logfile = stdout
+
 
     while true:
+        if reload_cfg:
+            echo "reloading " & cfg_name
+            #ban_game_servers() # TODO
+            #ban_clients() # TODO
+            #generate_banlist() # TODO
+            reload_cfg = false
+
         let c = await server.accept()
         clients.add c
 
 
-asyncCheck serve(port)
+let pc = paramCount() 
+
+var 
+    port = 42068
+    ip = ""
+    dir = ""
+
+if pc > 1:
+    dir = paramStr(2)
+if pc > 2:
+    discard parseInt(paramStr(3), port)
+if pc > 3:
+    ip = paramStr(4)
+
+asyncCheck serve(port, ip, dir)
 runForever()
